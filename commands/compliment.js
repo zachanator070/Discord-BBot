f1189dd570e6875d37b401e99ad427d2d260dd8d
Breyton Barber <breyton.barber@gmail.com>
2018-11-16 15:57:56 -0700

    Updating compliment and insult

--- commands/compliment.js
+++ commands/compliment.js
@@ -1,4 +1,13 @@
 module.exports = function (command) {
     let user_id = command.mentions.users.first().id;
-    command.channel.send(`You are beautiful <@${user_id}>!`)
+    let compliments = [
+        `You are beautiful <@${user_id}>!`,
+        `<@${user_id}>, you are the smartest person I know!`,
+        `You should be proud of yourself, <@${user_id}>`,
+        `You look great today <@${user_id}>!`,
+        `On a scale of 1 to 10, you are an 11 <@${user_id}>.`,
+        `If you were a triangle, you would be acute one <@${user_id}> :)`
+    ];
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    command.channel.send(compliments[randomIndex]);
 };
