845bc82d09eae7eef0193705643284fe252c1462
Breyton Barber <breyton.barber@gmail.com>
2018-11-16 16:08:16 -0700

    removing a period

--- commands/insult.js
+++ commands/insult.js
@@ -2,7 +2,7 @@ module.exports = function (command) {
 	let user_id = command.mentions.users.first().id;
 	let insults = [
 		`<@${user_id}> I fart in your general direction! Your mother was a hamster and your father smells of elderberries!`,
-		`<@${user_id}> is compensating for something 😏.`,
+		`<@${user_id}> is compensating for something 😏`,
 		`<@${user_id}> may look like an idiot and talk like an idiot but don't let that fool you. They really are an idiot.`,
 		`<@${user_id}>, you are a sad strange little man, and you have my pity.`,
 		`<@${user_id}> has no enemies, but is intensely disliked by their friends`,
